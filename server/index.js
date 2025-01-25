ed01ed5d61fc05e97e8a0916608862b1194cf038
Arunava Chatterjee <139641717+Arunava-Chatterjee@users.noreply.github.com>
2025-01-26 01:24:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,60 +5,34 @@ const EmployeeModel=require('./models/Employee')
 
 const app=express()
 app.use(express.json())
+app.use(cors())
 
-app.use(cors({
-    origin:[https://authentication-office-work.vercel.app],
-    methods:["POST","GET"],
-    credentials:true
-}));
 
-app.get("/",(req,res)=>{
-    res.json("Hello");
+mongoose.connect("mongodb://localhost:27017/employee");
+app.post("/login",(req,res)=>{
+    const {email,password}=req.body;
+    EmployeeModel.findOne({email:email})
+    .then(user => {
+        if(user){
+            if(user.password===password){
+                res.json("Success")
+            }
+            else{
+                res.json("Invalid Passwords")
+            }
+        }
+        else{
+            res.json("User not found")
+        }
+    })
 })
 
-mongoose.connect("mongodb+srv://Arun:Santana%407890@cluster1.fr9ag.mongodb.net/employee");
-app.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    EmployeeModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                if (user.password === password) {
-                    res.json("Success");
-                } else {
-                    res.status(401).json("Invalid Password");
-                }
-            } else {
-                res.status(404).json("User not found");
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json("An error occurred");
-        });
-});
-
-app.post("/register", (req, res) => {
-    const { name, email, password, mobileNo } = req.body;
-    EmployeeModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                res.status(400).json("User already exists");
-            } else {
-                const newUser = new EmployeeModel({ name, email, password, mobileNo });
-                newUser.save()
-                    .then(() => res.json("User registered successfully"))
-                    .catch(err => {
-                        console.log(err);
-                        res.status(500).json("An error occurred");
-                    });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json("An error occurred");
-        });
-});
+app.post('/register',(req,res)=>{
+    EmployeeModel.create(req.body)
+    .then(employees=>res.json(employees))
+    .catch(err=>res.json(err))
+})
 
 app.listen(3001,()=>{
     console.log("Server is running on port 3001")
-});
+})
